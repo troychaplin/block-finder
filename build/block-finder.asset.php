be6d491490eb431aac427f874df6aaf141a037ac
Troy Chaplin <troy.chaplin@gmail.com>
2026-06-13 20:08:20 -0400

    updated missing strings that could not be translated

--- build/block-finder.asset.php
+++ build/block-finder.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'cf35d7671913395eeda7');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '64315c8b412009bb0e8d');
